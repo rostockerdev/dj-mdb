5a4e6ad864fe2eb7948a103f7a140f9d9866d45e
rostockerdev <rostockerdev@gmail.com>
2021-04-09 23:26:26 +0600

    :seedling: Add: Added Postgres Admin Interface

--- postgresapp/admin.py
+++ postgresapp/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from postgresapp.models import POSTGRESModel
 
+class POSTGRESAdminArea(admin.AdminSite):
+    
+    site_header = 'POSTGRES Admin'
+    site_title = 'Postgres Admin Interface'
+
+postgres_site = POSTGRESAdminArea(name='POSTGRESAdmin')
+
+postgres_site.register(POSTGRESModel)
 admin.site.register(POSTGRESModel)
