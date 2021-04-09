c351433140b9f95481503939d8b88f5b008d8c35
rostockerdev <rostockerdev@gmail.com>
2021-04-10 00:13:53 +0600

    :wrench: Update: Update the Postgres Admin Panel Model Fields

--- postgresapp/admin.py
+++ postgresapp/admin.py
@@ -7,7 +7,13 @@ class POSTGRESAdminArea(admin.AdminSite):
     site_header = 'POSTGRES Admin'
     site_title = 'Postgres Admin Interface'
 
+class POSTGRESModelAdmin(admin.ModelAdmin):
+
+    fields = ['id', 'title', 'content']
+
+
+
 postgres_site = POSTGRESAdminArea(name='POSTGRESAdmin')
 
-postgres_site.register(POSTGRESModel)
-admin.site.register(POSTGRESModel)
+postgres_site.register(POSTGRESModel, POSTGRESModelAdmin)
+admin.site.register(POSTGRESModel, POSTGRESModelAdmin)
